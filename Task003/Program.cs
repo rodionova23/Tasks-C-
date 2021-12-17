6b94461c902d5702b0b0a66a9f04f9d8d9bbc727
Olga Rodionova <lelya_23@mail.ru>
2021-12-17 17:41:44 +0300

    task 003

---
+++ Task003/Program.cs
@@ -0,0 +1,32 @@
+﻿//  3. По заданному номеру дня недели вывести его название
+
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+switch (a)
+{
+    case 1:
+        Console.WriteLine("Понедельник");
+        break;
+    case 2:
+        Console.WriteLine("Вторник");
+        break;
+    case 3:
+        Console.WriteLine("Среда");
+        break;
+    case 4:
+        Console.WriteLine("Четверг");
+        break;
+    case 5:
+        Console.WriteLine("Пятница");
+        break;
+    case 6:
+        Console.WriteLine("Суббота");
+        break;
+    case 7:
+        Console.WriteLine("Воскресенье");    
+        break;                    
+    default:
+        Console.WriteLine("Введите другое число");
+        break;
+}
