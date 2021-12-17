79815ba47a13032eaec67feddc01df1c2b345e9e
Olga Rodionova <lelya_23@mail.ru>
2021-12-17 17:46:30 +0300

    task 004

---
+++ Task004/Program.cs
@@ -0,0 +1,14 @@
+﻿//  4. Найти максимальное из трех чисел
+
+int [] array = {105,5,25};
+int index = 0;
+int max = array [index];
+if(array[index+1]>max)
+{
+    max = array[index+1];
+}
+if(array[index+2]>max)
+{
+    max = array[index+2];
+}
+Console.WriteLine(max);
