9b09d10a0144abef0750212901c5d0334847b707
Olga Rodionova <lelya_23@mail.ru>
2021-12-18 18:42:26 +0300

    task 011 НЕ РАБОТАЕТ

---
+++ Task011/Program.cs
@@ -0,0 +1,15 @@
+﻿// 11. Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+int n = new Random().Next(10,100);
+Console.WriteLine(n);
+string num = Convert.ToString(n);
+int index = 0;
+if(num[index] > num [index++])
+{
+    Console.WriteLine(num[index]);
+
+}
+else
+{
+    Console.WriteLine(num[index++]);
+}
