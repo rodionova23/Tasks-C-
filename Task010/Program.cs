d2f52f7f86ff0e3ef4c88333a83f162412d2aa7a
Olga Rodionova <lelya_23@mail.ru>
2021-12-18 18:37:15 +0300

    task 010

---
+++ Task010/Program.cs
@@ -0,0 +1,11 @@
+﻿// 10. Показать вторую цифру трёхзначного числа
+
+int n = 864;
+Console.WriteLine(Num2(n));
+int Num2(int n)
+{
+  int a = n / 100;
+  int s = n - a * 100;
+  int b = s / 10;
+  return b;
+}
