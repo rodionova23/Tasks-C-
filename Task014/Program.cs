9d8b947d21b0be2a36d33c04a42f56b420767536
Olga Rodionova <lelya_23@mail.ru>
2021-12-18 18:52:36 +0300

    task 014

---
+++ Task014/Program.cs
@@ -0,0 +1,14 @@
+﻿// // 14. Найти третью цифру числа или сообщить, что её нет
+
+int numb = 179;
+if (numb < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+string number = Convert.ToString(numb);
+int index = 2;
+
+Console.WriteLine(number[index]);
+}
