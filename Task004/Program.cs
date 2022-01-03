56c2c7b506ac0f5f8c47f944b59f80c95e33619b
Olga Rodionova <lelya_23@mail.ru>
2022-01-03 17:40:08 +0300

    переделала в метод

--- Task004/Program.cs
+++ Task004/Program.cs
@@ -1,6 +1,8 @@
 ﻿//  4. Найти максимальное из трех чисел
 
-int [] array = {105,5,25};
+void Max3 (int a, int b, int c)
+{
+int [] array = {a,b,c};
 int index = 0;
 int max = array [index];
 if(array[index+1]>max)
@@ -12,3 +14,6 @@ if(array[index+2]>max)
     max = array[index+2];
 }
 Console.WriteLine(max);
+}
+
+Max3(10, 50 , 60);
