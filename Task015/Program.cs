c28c4e8a484b1d5722ea017955cc4d18a8fb5100
Olga Rodionova <lelya_23@mail.ru>
2022-01-05 21:21:38 +0300

    Решена задача 15

---
+++ Task015/Program.cs
@@ -0,0 +1,15 @@
+﻿//Дано число. Проверить кратно ли оно 7 и 23
+
+int a = 161;
+bool del7 = a % 7 == 0;
+bool del23 = a % 23 == 0;
+
+if (del7 && del23)
+{
+    Console.WriteLine("Число кратно 7 и 23");
+    
+}
+else
+{
+    Console.WriteLine("Число не кратно 7 и 23");
+}
